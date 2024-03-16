ca96e3ad471ee2ea0ba7d87343fd0fb872723ccb
MuazAsif-Dev <muazasif.info@gmail.com>
2024-03-16 17:57:29 -0400

    feat: add env validator config

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,22 @@
 import z from "zod";
 
-const envSchema = z.object({});
+const envClientSchema = z.object({
+	BASE_API_URL: z.string().url(),
+});
 
-export const env = envSchema.parse(process.env);
+const envServerSchema = z.object({
+	BASE_API_URL: z.string().url(),
+});
+
+const envClientObj = {
+	BASE_API_URL: process.env.NEXT_PUBLIC_BASE_API_URL,
+};
+
+const envServerObj = {
+	BASE_API_URL: process.env.BASE_API_URL,
+};
+
+export const env = {
+	client: envClientSchema.parse(envClientObj),
+	server: envServerSchema.parse(envServerObj),
+};
